c73ab6c556b29c037c8efaf3c2e920ca824ed6e7
Jonathan Niklasson Godar <jonathan.godar@gmail.com>
2023-04-22 16:29:58 +0200

    Hardcoded wol addresses but working

--- src/wol.rs
+++ src/wol.rs
@@ -23,25 +23,28 @@ pub async fn try_until_with_timeout(mac_addr: MacAddr, condition: impl Fn() -> b
 }
 
 pub async fn send(mac_addr: MacAddr) -> Result<(), Box<dyn std::error::Error>> {
-    let header = [255_u8; 6];
-    let mac_repeats: [u8; 6 * 16]  = mac_addr.0.into_iter().cycle().take(6 * 16).collect::<Vec<_>>().try_into().unwrap();
-
-    let payload: [u8; 102] = {
-        let mut payload = [0; 102];
-        let (one, two) = payload.split_at_mut(header.len());
-        one.copy_from_slice(&header);
-        two.copy_from_slice(&mac_repeats);
-        payload
-    };
-
-    println!("{:x?}", payload);
-    println!("{}", payload.len());
-
-    let socket = UdpSocket::bind("0.0.0.0:0").await?;
-    println!("Sending wol req");
-    socket.send_to(&payload, "0.0.0.0:9").await?;
-
+    println!("sending packet :D");
+    wake_on_lan::MagicPacket::new(&mac_addr.0).send_to("255.255.255.255:9", "192.168.1.223:0")?;
     Ok(())
+    // let header = [255_u8; 6];
+    // let mac_repeats: [u8; 6 * 16]  = mac_addr.0.into_iter().cycle().take(6 * 16).collect::<Vec<_>>().try_into().unwrap();
+
+    // let payload: [u8; 102] = {
+    //     let mut payload = [0; 102];
+    //     let (one, two) = payload.split_at_mut(header.len());
+    //     one.copy_from_slice(&header);
+    //     two.copy_from_slice(&mac_repeats);
+    //     payload
+    // };
+
+    // println!("{:x?}", payload);
+    // println!("{}", payload.len());
+
+    // let socket = UdpSocket::bind("192.168.1.223:0").await?;
+    // println!("Sending wol req");
+    // socket.send_to(&payload, "225.225.225.225:9").await?;
+
+    // Ok(())
 }
 
 
